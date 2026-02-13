fabfca489af5bf44cde7f9ad4322f7313ad446b7
Mahmoud <mahmoudnasser1561@gmail.com>
2026-02-14 01:00:43 +0200

    newDeck func

--- main.go
+++ main.go
@@ -1,13 +1,7 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Diamond", newCard()}
-	cards = append(cards, newCard())
+	cards := newDeck()
 
 	cards.print()
-
-}
-
-func newCard() string {
-	return "Five of Diamond"
 }
