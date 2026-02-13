45b519e6a1f15e95a4619c84d9df04c57c8a28db
Mahmoud <mahmoudnasser1561@gmail.com>
2026-02-14 00:43:33 +0200

    Basic deck type

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	cards := deck{"Ace of Diamond", newCard()}
+	cards = append(cards, newCard())
+
+	cards.print()
+
+}
+
+func newCard() string {
+	return "Five of Diamond"
+}
