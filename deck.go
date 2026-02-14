6c8a6c798dd78eb245ba087fce038ca9e3ff9019
Mahmoud <mahmoudnasser1561@gmail.com>
2026-02-14 14:42:33 +0200

    shuufle

--- deck.go
+++ deck.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
 )
@@ -53,4 +54,11 @@ func newDeckFromFile(filename string) deck {
 
 	s := strings.Split(string(bs), ",")
 	return deck(s)
+}
+
+func (d deck) shuffle() {
+	for i := range d {
+		newPos := rand.Intn(len(d) - 1)
+		d[i], d[newPos] = d[newPos], d[i]
+	}
 }
